24385058ea72423919ef5d17bdb9cba998e5444f
Clay <rpctropic13@gmail.com>
2024-08-28 00:52:48 +0000

    reworl

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for count in range(100):
-  if count < 100:
+  if count <= 98:
     print("{:02d}".format(count), end=", ")
   if count == 99:
     print("{:02d}".format(count), end="")
