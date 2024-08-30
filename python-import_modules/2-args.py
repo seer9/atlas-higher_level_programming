a3ab06b85ce7587e3abaf11ccec9f06e62d3f1bc
Clay <rpctropic13@gmail.com>
2024-08-30 22:18:17 +0000

    permissions

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,7 +2,7 @@
 if __name__ == "__main__":
     import sys
     l = len(sys.argv) - 1
-    
+
     if l == 0:
         print("{} arguments.".format(l))
     elif l == 1:
@@ -12,6 +12,6 @@ if __name__ == "__main__":
     if l >= 1:
         l = 0
         for args in sys.argv:
-          if l != 0:
-            print("{}: {}".format(l, args))
-          l += 1
+            if l != 0:
+                print("{}: {}".format(l, args))
+            l += 1
