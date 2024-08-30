39aa858b0d63eabe5b6d0520b653b539c6287cd0
Clay <rpctropic13@gmail.com>
2024-08-30 22:20:33 +0000

    variable name.

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    l = len(sys.argv) - 1
+    imp = len(sys.argv) - 1
 
-    if l == 0:
-        print("{} arguments.".format(l))
-    elif l == 1:
-        print("{} argument:".format(l))
+    if imp == 0:
+        print("{} arguments.".format(imp))
+    elif imp == 1:
+        print("{} argument:".format(imp))
     else:
-        print("{} arguments:".format(l))
-    if l >= 1:
-        l = 0
+        print("{} arguments:".format(imp))
+    if imp >= 1:
+        imp = 0
         for args in sys.argv:
-            if l != 0:
-                print("{}: {}".format(l, args))
-            l += 1
+            if imp != 0:
+                print("{}: {}".format(imp, args))
+            imp += 1
