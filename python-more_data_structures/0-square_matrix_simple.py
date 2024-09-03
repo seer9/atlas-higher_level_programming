dee9bfb8085f79934afeb97bae77591e4fab9fe9
Clay <rpctropic13@gmail.com>
2024-09-03 06:41:16 +0000

    new matrix

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -3,7 +3,7 @@ def square_matrix_simple(matrix=[]):
     new_matrix = []
     for row in matrix:
         new_row = []
-        for col in row:
-            new_row.append(col ** 2)
-            new_matrix.append(new_row)
+    for col in row:
+        new_row.append(col ** 2)
+        new_matrix.append(new_row)
     return new_matrix
