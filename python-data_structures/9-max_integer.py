e5d93e7c2991242d4be5508b675beda54b78f4bc
Clay <rpctropic13@gmail.com>
2024-09-03 05:33:03 +0000

    initial

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return
+
+    max = my_list[0]
+
+    for element in my_list:
+        if element > max:
+            max = element
+    return max
