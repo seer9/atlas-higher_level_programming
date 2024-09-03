f68df228a096c2f3344e0c9248cc1d364ec6c74b
Clay <rpctropic13@gmail.com>
2024-09-03 00:02:00 +0000

    typo

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    if i < 0 or i > len(my_list):
+    if idx < 0 or idx > len(my_list):
         return my_list
     else:
         my_list[i] = element
