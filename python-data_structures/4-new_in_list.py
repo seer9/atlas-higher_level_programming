1df334c2d818f579ce5d8150455caa0bbd453703
Clay <rpctropic13@gmail.com>
2024-09-03 00:00:50 +0000

    initial

---
+++ python-data_structures/4-new_in_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    if i < 0 or i > len(my_list):
+        return my_list
+    else:
+        my_list[i] = element
+    return my_list
+  
