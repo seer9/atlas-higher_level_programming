55b0eeeef17dbe353672d8ac827d4c7e44266e89
Clay <rpctropic13@gmail.com>
2024-09-03 06:01:08 +0000

    append to new list

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -2,8 +2,8 @@
 def divisible_by_2(my_list=[]):
     new_list = []
     for index in range(len(my_list)):
-      if my_list[index] % 2 == 0:
-        new_list.append(True)
-      else:
-        new_list.append(False)
+        if my_list[index] % 2 == 0:
+            new_list.append(True)
+        else:
+            new_list.append(False)
     return new_list
