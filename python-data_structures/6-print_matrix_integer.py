a9413d29d1d8cb72327f0ba4b58ebf7429c675f9
Clay <rpctropic13@gmail.com>
2024-09-03 00:50:39 +0000

    space check

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
+#!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        for col in row:
-            print(" ".join("{:d}".format(col)) for col in row)
-        print()
+        print(" ".join("{:d}".format(col) for col in row))
