36fa57111ffbac35f5bc5e6282e940d88b222ec5
Clay <rpctropic13@gmail.com>
2024-09-03 00:44:52 +0000

    initial

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,4 +2,5 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for col in row:
-            print("{:d}".format(col), end=" ")
+            print(" ".join("{:d}".format(col)))
+        print()
