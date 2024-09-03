7c10014d55bf4bfe47037eb4503a1bb9e5102c8c
Clay <rpctropic13@gmail.com>
2024-09-03 05:46:29 +0000

    out of range

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
     new_list = []
-    for num in my_list:
-        if num % 2 == 0:
-            new_list[num] = True
-        else:
-            new_list[num] = False
+    for num in range(len(my_list)):
+      if num % 2 == 0:
+        new_list[num] = True
+      else:
+        new_list[num] = False
     return new_list
