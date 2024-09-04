0e5770dfde91c2b5b4a2c935326df67d22a6665a
Clay <rpctropic13@gmail.com>
2024-09-04 04:30:02 +0000

    check

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -4,7 +4,7 @@ def safe_print_list(my_list=[], x=0):
     try:
         for idx in range(x):
             print("{}".format(my_list[idx], end=""))
-          idx += 1
+            idx += 1
     except IndexError:
         pass
     print()
