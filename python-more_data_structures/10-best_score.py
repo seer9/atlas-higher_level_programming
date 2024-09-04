3b4d68fb8f7cf9d525a6f988b82f108bb031dbc4
Clay <rpctropic13@gmail.com>
2024-09-04 03:51:17 +0000

    check

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if a_dictionary:
-        return (max(a_dictionary))
+        return max(a_dictionary.values())
     else:
         return None
