dfc22a3bd62d6a907c2d22e974c75119da34a7fb
Clay <rpctropic13@gmail.com>
2024-09-04 04:27:16 +0000

    check

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -3,6 +3,7 @@ def safe_print_list(my_list=[], x=0):
     try:
         for idx in range(x):
             print("{}".format(my_list[idx], end=""))
+          idx += 1
     except IndexError:
         pass
     print()
