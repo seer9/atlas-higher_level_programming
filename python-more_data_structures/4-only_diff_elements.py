6090292a1ae5a9d5e6cc9983749f1de80d947372
Clay <rpctropic13@gmail.com>
2024-09-04 02:27:32 +0000

    initial

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
 #now i understand what it means to code high up
-    return (set_1 ^ set_2)
+  
+#or return (set_1 ^ set_2))
+    return set_1.symatric_difference(set_1, set_2)
