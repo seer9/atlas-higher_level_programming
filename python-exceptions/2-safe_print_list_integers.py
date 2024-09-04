e2c3663ac310438edf7804a4f755708cba8692cc
Clay <rpctropic13@gmail.com>
2024-09-04 04:49:46 +0000

    print list ints

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -3,9 +3,9 @@ def safe_print_list_integers(my_list=[], x=0):
     position = 0
     for idx in range(x):
         try:
-            print("{:d}".format(my_list[position]), end="")
+            print("{}".format(my_list[position]), end="")
             position += 1
-        except IndexError:
+        except (ValueError, TypeError):
             continue
-            print()
-            return position
+    print()
+    return position
