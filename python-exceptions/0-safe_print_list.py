633cdeaab9292390728c24c9c6396b7c949aaeeb
Clay <rpctropic13@gmail.com>
2024-09-04 04:28:06 +0000

    check

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    try:
+    idx = 0
+  try:
         for idx in range(x):
             print("{}".format(my_list[idx], end=""))
           idx += 1
