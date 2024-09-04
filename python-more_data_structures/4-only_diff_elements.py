2a8093ac733937e9f2d7af7fbf85b8c0120a1ea4
Clay <rpctropic13@gmail.com>
2024-09-04 02:36:45 +0000

    in only one set

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-
 def only_diff_elements(set_1, set_2):
-  #finally programming high up
+##finally programming high up
 
-  #or return (set_1 ^ set_2)
+##or return (set_1 ^ set_2)
     return list(set_1.symmetric_difference(set_2))
