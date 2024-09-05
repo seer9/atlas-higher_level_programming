8407164878f1a71127a41917650813ad7ccb3ab6
Clay <rpctropic13@gmail.com>
2024-09-05 23:38:26 +0000

    error raising

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,15 +1,18 @@
-#!/usr/bin/python3
-"""class Square:"""
+##!/usr/bin/python3
+"""Module for Square class"""
 
 
 class Square:
+    """A class that defines a square"""
 
-  def __init__(self, size=0):
-    """Initializes a square object with a size"""
+    def __init__(self, size=0):
+        """Initialize the square
 
-    
-    if type(size) is not int:
-      raise TypeError("size must be an integer")
-    if size < 0:
-      raise ValueError("size must be >= 0")
-    self.__size = size
+        Args:
+            size (int): The size of the square. Defaults to 0.
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
