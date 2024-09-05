149d783f599f3280e3e2657186796d3d6da5ca3f
Clay <rpctropic13@gmail.com>
2024-09-05 23:40:59 +0000

    error raising

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -6,7 +6,7 @@ class Square:
     """A class that defines a square"""
 
     def __init__(self, size=0):
-        
+
         if type(size) is not int:
             raise TypeError("size must be an integer")
         if size < 0:
