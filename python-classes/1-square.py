99ef12093938f66f2306d30a7d7a610c57a1d2f9
Clay <rpctropic13@gmail.com>
2024-09-05 22:53:05 +0000

    initial

---
+++ python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""represents a square"""
+class Square:
+  """square"""
+  def __init__(self, size):
+    """initializes a square"""
+    self.__size = size
+    """returns the size of the square"""
+    return self.__size
