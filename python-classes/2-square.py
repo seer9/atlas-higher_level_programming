6178f123a7c9765e2fe141255e209f6e4a5f8cea
Clay <rpctropic13@gmail.com>
2024-09-05 23:31:38 +0000

    initial

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -7,6 +7,7 @@ class Square:
   def __init__(self, size=0):
     """Initializes a square object with a size"""
 
+    
     if type(size) is not int:
       raise TypeError("size must be an integer")
     if size < 0:
