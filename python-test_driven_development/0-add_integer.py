3fe178476d7e71137c6953792fca190245b9903a
Clay <rpctropic13@gmail.com>
2024-09-09 19:33:56 +0000

    a as a float case

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -6,8 +6,9 @@ def add_integer(a, b=98):
     """
     Function that adds two integers
     """
-    if not isinstance(a, (int, float)):
+    if not isinstance(a, int) or not isinstance(a, float):
         raise TypeError("a must be an integer")
-    if not isinstance(b, (int, float)):
+    if not isinstance(b, int) or not isinstance(a, float):
         raise TypeError("b must be an integer")
+
     return a + b
