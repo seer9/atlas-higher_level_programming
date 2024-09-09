c890a10ac18f492b9f8ed1d4c9018323d032c9d3
Clay <rpctropic13@gmail.com>
2024-09-09 19:22:15 +0000

    doc

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 
 
-"""Module for add_integer function"""
+"""add_integer function"""
 def add_integer(a, b=98):
+    """
+    Function that adds two integers
+    """
     if type(a) is not int:
         raise TypeError("a must be an integer")
     if type(b) is not int:
