6778a00d3db6c34c38c6e529cb185f00376afa36
Clay <rpctropic13@gmail.com>
2024-09-10 21:58:55 +0000

    try again

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -3,12 +3,12 @@
 
 
 def text_indentation(text):
-    """functions to space text"""
+        """functions to space text"""
 
-    if not isinstance(text, str):
-        raise TypeError("text must be a string")
+        if not isinstance(text, str):
+                raise TypeError("text must be a string")
 
-    for char in text:
-        print("{}".format(char))
-        if char in ".:?":
-                print("\n\n", end="")
+        for char in text:
+                print("{}".format(char))
+                if char in ".:?":
+                        print("\n\n", end="")
