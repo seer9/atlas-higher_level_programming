086358ecd82da8402e2492f1d2ade1b52c1aa8a0
Clay <rpctropic13@gmail.com>
2024-09-10 22:48:37 +0000

    fixing code indentation

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,20 +1,18 @@
 #!/usr/bin/python3
-"""the text_indentation module"""
-
+"""Module for text_indentation function"""
 
 def text_indentation(text):
-	"""functions to space text"""
+		"""indents text depending on the char position"""
+		if not isinstance(text, str):
+				raise TypeError("text must be a string")
 
-	if not isinstance(text, str):
-		raise TypeError("text must be a string")
-
-	lead = 0
-	for char in text:
-		if lead and char == " ":
-			continue
-		print(char, end="")
-		if char in ".?:":
-				print("\n")
-				lead = 1
-		else:
-				lead = 0
+		lead = 0
+		for char in text:
+				if lead and char == ' ':
+						continue
+				print(char, end="")
+				if char in ".?:":
+						print("\n")
+						lead = 1
+				else:
+						lead = 0
