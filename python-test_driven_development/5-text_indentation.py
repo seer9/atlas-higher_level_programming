3fd0d5242752771aa8847274b26edf900ca9cc55
Clay <rpctropic13@gmail.com>
2024-09-10 21:36:42 +0000

    initial

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""the text_indentation module"""
+
+
+def text_indentation(text):
+    """functions to space text"""
+
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    for char in text:
+        print(char, end="")
+        if char in ".?:":
+            print("\n\n", end="")
