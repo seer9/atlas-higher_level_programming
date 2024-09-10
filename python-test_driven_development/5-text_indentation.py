88e782c7dcc7ec6e0486c89e7d126137dbddc3de
Clay <rpctropic13@gmail.com>
2024-09-10 22:53:13 +0000

    check

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -2,17 +2,17 @@
 """Module for text_indentation function"""
 
 def text_indentation(text):
-		"""indents text depending on the char position"""
-		if not isinstance(text, str):
-				raise TypeError("text must be a string")
+        """indents text depending on the char position"""
+        if not isinstance(text, str):
+                raise TypeError("text must be a string")
 
-		lead = 0
-		for char in text:
-				if lead and char == ' ':
-						continue
-				print(char, end="")
-				if char in ".?:":
-						print("\n")
-						lead = 1
-				else:
-						lead = 0
+        lead = 0
+        for char in text:
+                if lead and char == ' ':
+                        continue
+                print(char, end="")
+                if char in ".?:":
+                        print("\n")
+                        lead = 1
+                else:
+                        lead = 0
