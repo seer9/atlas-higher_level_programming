9b64a8ecf4220c6835b2ac50f8c3cd7dfa3cba8b
Clay <rpctropic13@gmail.com>
2024-09-10 22:01:09 +0000

    maybe newline

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -9,6 +9,5 @@ def text_indentation(text):
                 raise TypeError("text must be a string")
 
         for char in text:
-                print("{}".format(char))
-                if char in ".:?":
-                        print("\n\n", end="")
+                print(char, end="")
+        print() 
