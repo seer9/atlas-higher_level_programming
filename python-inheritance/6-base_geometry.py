4746fede8061be92235cea3b5548c403aab919e0
Clay <rpctropic13@gmail.com>
2024-09-16 20:28:00 +0000

    exception if not implemented

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""base_geometry class"""
+
+
+class BaseGeometry:
+    """CLASS"""
+
+    def area(self):
+        """Returns the area of the geometry"""
+        
+        raise Exception("area() is not implemented")
