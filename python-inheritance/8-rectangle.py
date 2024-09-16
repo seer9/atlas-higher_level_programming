45bf5cb91a699d51a70af71566cf1005e5f717ea
Clay <rpctropic13@gmail.com>
2024-09-16 21:23:46 +0000

    pepcheck

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -9,7 +9,7 @@ class Rectangle(BaseGeometry):
     """retangle class inherits from BaseGeometry"""
 
     def __init__(self, width, height):
-      self.integer_validator("width", width)
-      self.integer_validator("height", height)
-      self.__width = width
-      self.__height = height
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
