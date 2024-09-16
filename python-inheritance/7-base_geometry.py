e1a1c4e0169dbdd428e27b2030f491e813346948
Clay <rpctropic13@gmail.com>
2024-09-16 20:40:45 +0000

    validates that value is int

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""base_geometry class"""
+
+
+class BaseGeometry:
+    """CLASS"""
+
+    def area(self):
+        """Returns the area of the geometry"""
+
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validates that value is an integer"""
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be > 0".format(name))
