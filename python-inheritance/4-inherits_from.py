f5cc23cff6e901e189d100f4aab27a646f8ef60f
Clay <rpctropic13@gmail.com>
2024-09-16 20:17:36 +0000

    checks inheritence

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""inherits_from module"""
+
+
+def inherits_from(obj, a_class):
+    """returns True if the object is an instance of a class
+    that inherited (directly or indirectly) from the specified class; else False."""
+
+    if isinstance(obj, a_class) && type(obj) == a_class:
+        return True
+    else:
+        return False
