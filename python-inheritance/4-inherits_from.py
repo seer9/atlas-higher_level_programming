f7023afd2bfe7ad81878d61bff51eb437207ad51
Clay <rpctropic13@gmail.com>
2024-09-16 20:20:31 +0000

    swap

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -8,6 +8,6 @@ def inherits_from(obj, a_class):
 
     
     if isinstance(obj, a_class) and type(obj) == a_class:
-        return True
-    else:
         return False
+    else:
+        return True
