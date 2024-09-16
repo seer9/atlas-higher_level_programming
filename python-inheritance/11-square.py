f86a2e4c777b1f492e643afe2ecdcf103435fd1b
Clay <rpctropic13@gmail.com>
2024-09-16 21:47:41 +0000

    add string representation

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Square class"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """inherits from Rectangle"""
+
+    def __init__(self, size):
+        """initialization"""
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """area"""
+        return (self.__size ** 2)
+
+    def __str__(self):
+        """string representation"""
+
+        return "[Square] {}/{}".format(self.__size, self.size)
