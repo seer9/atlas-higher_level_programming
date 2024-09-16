8206dfd3c589e33f3753755ae79dec0eed787733
Clay <rpctropic13@gmail.com>
2024-09-16 21:07:10 +0000

    validates that value is an int > 0

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -12,7 +12,7 @@ class BaseGeometry:
 
     def integer_validator(self, name, value):
         """Validates that value is an integer"""
-        
+
         if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
