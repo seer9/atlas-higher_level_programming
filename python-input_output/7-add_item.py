1912825c655baa3b4297458d0018dc8862f45ec2
Clay <rpctropic13@gmail.com>
2024-09-17 19:24:42 +0000

    mod doc

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -2,16 +2,14 @@
 """add_item function"""
 import sys
 
+load = __import__('6-load_from_json_file').load_from_json_file
 
-    load = __import__('6-load_from_json_file').load_from_json_file
-
-    save = __import__('5-save_to_json_file').save_to_json_file
-    filename = "add_item.json"
-  
-    try:
+save = __import__('5-save_to_json_file').save_to_json_file
+filename = "add_item.json"
+try:
         data = load_from_json_file(filename)
-    except FileNotFoundError:
+except FileNotFoundError:
         data = []
-
-    items.extend(sys.argv[1:])
-    save_to_json_file(data, filename)
+    
+        items.extend(sys.argv[1:])
+        save_to_json_file(data, filename)
