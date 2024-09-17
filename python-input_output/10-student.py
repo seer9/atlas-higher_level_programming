bd5a6572bd91bba41cb15d9784eb79881ebd6948
Clay <rpctropic13@gmail.com>
2024-09-17 20:15:59 +0000

    check

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -4,21 +4,20 @@
 
 class Student:
 
-        def __init__(self, first_name, last_name, age):
-                """initializes a student"""
+    def __init__(self, first_name, last_name, age):
+        """initializes a student"""
 
-                self.first_name = first_name
-                self.last_name = last_name
-                self.age = age
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
-        def to_json(self, attrs=None):
-                """returns a json representation of the student"""
-                dict = {}
-                i = 0
-                if type(attrs) is list:
-                    for attr in attrs:
-                        if attr in self.__dict__:
-                            dict[attr] = self.__dict__[attr]
-                return (dict)  
-                else:
-                return (self.__dict__)
+    def to_json(self, attrs=None):
+        """returns a json representation of the student"""
+        dict = {}
+        if type(attrs) is list:
+            for attr in attrs:
+                if attr in self.__dict__:
+                    dict[attr] = self.__dict__[attr]
+            return (dict)  
+        else:
+            return (self.__dict__)
