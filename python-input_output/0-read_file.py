eff5055e4964a544297d5e470c909b964a5a2d41
Clay <rpctropic13@gmail.com>
2024-09-17 15:53:13 +0000

    initial

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""read_file function"""
+
+
+def read_file(filename=""):
+    """reads a file and returns its content"""
+
+    with open(filename, encoding="utf-8") as f:
+        return (f.read(), end=None) 
