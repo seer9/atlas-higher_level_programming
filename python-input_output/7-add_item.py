3201a14f564cbbd86ac05ca70818ee5707f90319
Clay <rpctropic13@gmail.com>
2024-09-17 18:28:52 +0000

    initial

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""add_item function"""
+
+
+from sys import argv
+
+load = __import__('6-load_from_json_file').load_from_json_file
+save = __import__('5-save_to_json_file').save_to_json_file
