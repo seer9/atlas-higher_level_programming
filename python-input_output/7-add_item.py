bb6e121309506a3b3f402045d011d35aa10db5a3
Clay <rpctropic13@gmail.com>
2024-09-17 19:37:35 +0000

    load, add, save contents

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -7,10 +7,10 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 filename = "add_item.json"
 try:
-        data = load_from_json_file(filename)
+    data = load_from_json_file(filename)
 except FileNotFoundError:
-        data = []
+    data = []
 
 for _ in range(1, len(argv)):
-        data.append(argv[_])
-        save_to_json_file(data, filename)
+    data.append(argv[_])
+    save_to_json_file(data, filename)
