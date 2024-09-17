baaf344134669912a4d32c8d6c14e636a3fe8c15
Clay <rpctropic13@gmail.com>
2024-09-17 18:48:24 +0000

    initial

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -6,3 +6,14 @@ from sys import argv
 
 load = __import__('6-load_from_json_file').load_from_json_file
 save = __import__('5-save_to_json_file').save_to_json_file
+
+filename = "add_item.json"
+
+try:
+    data = load_from_json_file(filename)
+except FileNotFoundError:
+    data = []
+
+if len(argv) > 1:
+    data.append(argv[1])
+save_to_json_file(data, filename)
