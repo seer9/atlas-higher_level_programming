b96373bf01f0657dd5f3154389514e6e38be9432
Clay <rpctropic13@gmail.com>
2024-09-17 19:04:02 +0000

    check

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """add_item function"""
-
-
 from sys import argv
+
+if __name__ == "__main__":
 
 load = __import__('6-load_from_json_file').load_from_json_file
 save = __import__('5-save_to_json_file').save_to_json_file
