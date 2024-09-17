c0ef54173a568cac618d793fb7c0214957b20046
Clay <rpctropic13@gmail.com>
2024-09-17 19:46:06 +0000

    class to define a student

---
+++ python-input_output/9-student.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""student class"""
+
+
+class Student:
+
+
+    def __init__(self, first_name, last_name, age):
+        """initializes a student"""
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """returns a json representation of the student"""
+
+        return (obj.__dict__)
