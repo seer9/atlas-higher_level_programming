aeee087a816636b4b693aa52c9cf275d271b6235
Clay <rpctropic13@gmail.com>
2024-09-17 19:28:17 +0000

    check

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -2,9 +2,9 @@
 """add_item function"""
 import sys
 
-load = __import__('6-load_from_json_file').load_from_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-save = __import__('5-save_to_json_file').save_to_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 filename = "add_item.json"
 try:
         data = load_from_json_file(filename)
