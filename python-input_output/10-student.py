971da9d3b11bf7bc0ae402b1fe2ed06c79e24693
Clay <rpctropic13@gmail.com>
2024-09-17 20:12:56 +0000

    check

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -5,7 +5,7 @@
 class Student:
 
         def __init__(self, first_name, last_name, age):
-        """initializes a student"""
+                """initializes a student"""
 
                 self.first_name = first_name
                 self.last_name = last_name
@@ -20,5 +20,5 @@ class Student:
                         if attr in self.__dict__:
                             dict[attr] = self.__dict__[attr]
                 return (dict)  
-        else:
+                else:
                 return (self.__dict__)
