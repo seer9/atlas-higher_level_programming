2d267c197e1df3bb8a2f1b05a5d3e4297f0ad5f6
Clay <rpctropic13@gmail.com>
2024-09-17 19:33:25 +0000

    check

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """add_item function"""
-import sys
+from sys import argv
 
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
@@ -10,6 +10,7 @@ try:
         data = load_from_json_file(filename)
 except FileNotFoundError:
         data = []
-    
+
+for _ in range(1, len(argv))
         data.extend(sys.argv[1:])
         save_to_json_file(data, filename)
