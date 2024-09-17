4ff2e7ad4b49e70ff2d0693fc054fa2d1f9bbd77
Clay <rpctropic13@gmail.com>
2024-09-17 19:17:28 +0000

    check

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -3,17 +3,16 @@
 from sys import argv
 
 if __name__ == "__main__":
+    load = __import__('6-load_from_json_file').load_from_json_file
 
-load = __import__('6-load_from_json_file').load_from_json_file
-save = __import__('5-save_to_json_file').save_to_json_file
+    save = __import__('5-save_to_json_file').save_to_json_file
+    filename = "add_item.json"
+  
+    try:
+        data = load_from_json_file(filename)
+    except FileNotFoundError:
+        data = []
 
-filename = "add_item.json"
-
-try:
-    data = load_from_json_file(filename)
-except FileNotFoundError:
-    data = []
-
-for _ in range(1, len(argv)):
-    data.append(argv[_])
-save_to_json_file(data, filename)
+    for _ in range(1, len(argv)):
+        data.append(argv[_])
+    save_to_json_file(data, filename)
