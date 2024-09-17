eec843b8f59f193716679674e8748740d5f53c5e
Clay <rpctropic13@gmail.com>
2024-09-17 20:04:50 +0000

    initial

---
+++ python-input_output/10-student.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""student class"""
+
+
+class Student:
+
+        def __init__(self, first_name, last_name, age):
+                """initializes a student"""
+
+                self.first_name = first_name
+                self.last_name = last_name
+                self.age = age
+
+        def to_json(self, attrs=None):
+                """returns a json representation of the student"""
+            dict = {}
+            i = 0
+            if type(attrs) is list:
+                for attr in attrs:
+                    if attr in self.__dict__:
+                        dict[attr] = self.__dict__[attr]
+                return (dict)  
+            else:
+                return (self.__dict__)
