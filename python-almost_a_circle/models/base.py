23241a7d9e530992382d5584ea80fafa5801a2ae
Clay <rpctropic13@gmail.com>
2024-09-23 23:27:16 +0000

    check

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
 """This is the file for base class"""
+import json
 
 
 class Base:
-        """class base"""
-        __nb_objects = 0
+    """class base"""
+    __nb_objects = 0
 
-        def __init__(self, id=None):
-            """increments the id #"""
+    def __init__(self, id=None):
+        """increments the id #"""
 
-            if id is not None:
-                self.id = id
-            else:
-                Base.__nb_objects += 1
-                self.id = Base.__nb_objects
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
