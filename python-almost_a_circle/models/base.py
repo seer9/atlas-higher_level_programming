ce288f692a059bc47edda7d3a00c5c83a1716be9
Clay <rpctropic13@gmail.com>
2024-09-24 03:40:14 +0000

    method that creates a json string representation of an obj list

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -16,6 +16,17 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """saves a list of objects to a file"""
+        file_name = cls.__name__ + ".json"
+        if list_objs is None:
+            return []
+        else:
+            list = [obj.to_dictionary() for obj in list_objs]
+        with open(file_name, "w") as file:
+            file.write(cls.to_json_string(list))
+
     @staticmethod
     def to_json_string(list_dictionaries):
         """Converts an object to a JSON string."""
