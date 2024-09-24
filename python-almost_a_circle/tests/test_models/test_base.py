69bcdbd52932a9a3888cd5ff14752ea7549bfd78
Clay <rpctropic13@gmail.com>
2024-09-24 01:19:57 +0000

    test case

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -6,23 +6,31 @@ from models.base import Base
 class TestBase(unittest.TestCase):
 
     def test_id(self):
-       
+
         base = Base()
         self.assertIsNotNone(base.id)
 
     def test_auto_assing_id(self):
-        
+
         b1 = Base()
         b2 = Base()
         self.assertNotEqual(b1.id, b2.id)
 
     def test_passed_id(self):
-        
+
         base = Base(89)
         self.assertEqual(base.id, 89)
 
-    def test_to_json_string_none(self):
-      
+    def test_to_json_string(self):
+
         base_inst = Base()
         string = base_inst.to_json_str(None)
+        self.assertEqual(string, '[]')
+
+    def test_to_json_string_normal(self):
+        string = Base.to_json_str([{'id': 12}])
+        self.assertEqual(string, '[{"id": 12}]')
+
+    def test_from_json_string(self):
+        string = Base.to_json_str([])
         self.assertEqual(string, '[]')
