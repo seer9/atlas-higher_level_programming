f8955028a74422ec9bfb3db00cf045ee3a6d4367
Clay <rpctropic13@gmail.com>
2024-09-24 02:07:29 +0000

    check

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -16,7 +16,8 @@ class Rectangle(Base):
 
     def __str__(self) -> str:
         """Return the readable string rep of the Rectangle."""
-        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.__x, self.__y, self.__width, self.height)
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(
+          self.id, self.__x, self.__y, self.__width, self.height)
 
     @property
     def width(self):
