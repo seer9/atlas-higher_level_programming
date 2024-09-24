ff81959e425cb797ff0b8da9eb942ee634411e2c
Clay <rpctropic13@gmail.com>
2024-09-24 01:34:18 +0000

    added property to method from json string

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -22,3 +22,9 @@ class Base:
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return '[]'
         return json.dumps(list_dictionaries)
+
+    @staticmethod
+    def from_json_string(string):
+        if string is None or len(string) == 0:
+            return []
+        return json.loads(string)
