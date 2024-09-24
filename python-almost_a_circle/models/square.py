6e8a4aced2f70276b215945fa6bd671cb67abee8
Clay <rpctropic13@gmail.com>
2024-09-24 03:16:14 +0000

    check

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -4,27 +4,27 @@ from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-  """square class"""
-  def __init__(self, size, x=0, y=0, id=None):
-    """initializes a square"""
-    super().__init__(size, size, x, y, id)
+    """square class"""
+    def __init__(self, size, x=0, y=0, id=None):
+        """initializes a square"""
+        super().__init__(size, size, x, y, id)
 
-  def __str__(self):
-    """returns a string representation of the square"""
-    return "[Square] ({}) {}/{} - {}".format(
-      self.id, self.x, self.y, self.width)
+    def __str__(self):
+        """returns a string representation of the square"""
+        return "[Square] ({}) {}/{} - {}".format(
+        self.id, self.x, self.y, self.width)
 
-  def update(self, *args, **kwargs):
-    """Update the Square attributes."""
-    if args:
-        attrs = ["id", "size", "x", "y"]
-        for i, value in enumerate(args):
-            if i < len(attrs):
-              setattr(self, attrs[i], args[i])
-    else:
+    def update(self, *args, **kwargs):
+        """Update the Square attributes."""
+        if args:
+            attrs = ["id", "size", "x", "y"]
+            for i, value in enumerate(args):
+                if i < len(attrs):
+                  setattr(self, attrs[i], args[i])
+        else:
 
-        for key, value in kwargs.items():
-              setattr(self, key, value)
+            for key, value in kwargs.items():
+                  setattr(self, key, value)
 
   @property
   def size(self):
