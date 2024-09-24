f1d913adeafcd2e8d3b08984af6d7dd4fd2a7658
Clay <rpctropic13@gmail.com>
2024-09-24 01:59:28 +0000

    check

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -3,9 +3,9 @@
 from models.base import Base
 
 
-"""Rectangle inherits from Base"""
 class Rectangle(Base):
     """initializes a rectangle"""
+
 
     def __init__(self, width=0, height=0, x=0, y=0, id=None):
         """initializes a rectangle"""
