63480de46138f431a5026e95058898cf1e18f7f1
Clay <rpctropic13@gmail.com>
2024-09-24 03:24:27 +0000

    dictionary representation of rectangle

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -45,6 +45,15 @@ class Rectangle(Base):
             for key, value in kwargs.items():
                 setattr(self, key, value)
 
+    def to_dictionary(self):
+      """returns a dictionary representation of the square"""
+      return {
+        'id': self.id,
+        'width': self.__width,
+        'height': self.__height,
+        'x': self.__x,
+        'y': self.__y
+      }
     @property
     def width(self):
         """Getter method for width."""
