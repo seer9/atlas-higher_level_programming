59616442cdd0d2b5ae24363774a44cf285964928
Clay <rpctropic13@gmail.com>
2024-09-24 03:12:48 +0000

    pepcheck

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -4,35 +4,35 @@ from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """square class"""
-    def __init__(self, size, x=0, y=0, id=None):
-        """initializes a square"""
-        super().__init__(size, size, x, y, id)
+  """square class"""
+  def __init__(self, size, x=0, y=0, id=None):
+    """initializes a square"""
+    super().__init__(size, size, x, y, id)
 
-    def __str__(self):
-        """returns a string representation of the square"""
-        return "[Square] ({}) {}/{} - {}".format(
-            self.id, self.x, self.y, self.width)
+  def __str__(self):
+    """returns a string representation of the square"""
+    return "[Square] ({}) {}/{} - {}".format(
+      self.id, self.x, self.y, self.width)
 
-    def update(self, *args, **kwargs):
-        """Update the Square attributes."""
-        if args:
-                attrs = ["id", "size", "x", "y"]
-                for i, value in enumerate(args):
-                    if i < len(attrs):
-                         setattr(self, attrs[i], args[i])
-        else:
+  def update(self, *args, **kwargs):
+    """Update the Square attributes."""
+    if args:
+        attrs = ["id", "size", "x", "y"]
+        for i, value in enumerate(args):
+            if i < len(attrs):
+              setattr(self, attrs[i], args[i])
+    else:
 
-                for key, value in kwargs.items():
-                        setattr(self, key, value)
+        for key, value in kwargs.items():
+              setattr(self, key, value)
 
-    @property
-    def size(self):
-        """getter method for size"""
-        return self.width
+  @property
+  def size(self):
+    """getter method for size"""
+    return self.width
 
-    @size.setter
-    def size(self, value):
-        """setter method for size"""
-        self.width = value
-        self.height = value
+  @size.setter
+  def size(self, value):
+    """setter method for size"""
+    self.width = value
+    self.height = value
