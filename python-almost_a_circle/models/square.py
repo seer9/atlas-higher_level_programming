9f877c3332a675b8cc5bb8030463ffbda9367159
Clay <rpctropic13@gmail.com>
2024-09-24 02:38:49 +0000

    class that represents a sqaure

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""square class"""
+from models.rectangle import Rectangle
+
+class Square(Rectangle):
+    """square class"""
+    def __init__(self, size, x=0, y=0, id=None):
+        """initializes a square"""
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """returns a string representation of the square"""
+        return "[Square] ({}) {}/{} - {}/{}".format(
+          self.id, self.x, self.y, self.width, self.height)
