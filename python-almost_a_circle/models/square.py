f8ca89e2fa9807a648527d9722a7213943a4215a
Clay <rpctropic13@gmail.com>
2024-09-24 02:41:28 +0000

    changed formatting for being overloaded

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -10,5 +10,5 @@ class Square(Rectangle):
 
     def __str__(self):
         """returns a string representation of the square"""
-        return "[Square] ({}) {}/{} - {}/{}".format(
-          self.id, self.x, self.y, self.width, self.height)
+        return "[Square] ({}) {}/{} - {}".format(
+          self.id, self.x, self.y, self.height)
