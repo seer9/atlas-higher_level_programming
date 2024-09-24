70738d9a7ef8c1ec31623a0c6103b0d8ad6c77a4
Clay <rpctropic13@gmail.com>
2024-09-24 00:21:22 +0000

    test case

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
 import unittest
 from models.base import Base
-import json
 
 
 class TestBase(unittest.TestCase):
-    """Test class for Base"""
 
     def test_id(self):
-        """Testing for auto assign id"""
         base = Base()
         self.assertIsNotNone(base.id)
