c4ec3ba1e394345d4173c5858c7245a233bb4c84
Clay <rpctropic13@gmail.com>
2024-09-24 03:03:10 +0000

    check

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -12,7 +12,7 @@ class Square(Rectangle):
     def __str__(self):
         """returns a string representation of the square"""
         return "[Square] ({}) {}/{} - {}".format(
-          self.id, self.x, self.y, self.height)
+          self.id, self.x, self.y, self.width)
 
     def update(self, *args, **kwargs):
         """Update the Square attributes."""
