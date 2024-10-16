59a4f6b68d1b4c4748454e8cbfbd4a7de6817e1f
clay <rpctropic13@gmail.com>
2024-10-16 01:27:10 +0000

    added specifiers to select statement

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -1,5 +1,5 @@
 -- lists all shows, and all genres linked to a show
-SELECT tv_shows.title, tv_genres.name
+SELECT tv_shows.title AS title, tv_genres.name AS name
 FROM tv_shows
 LEFT JOIN tv_genres ON tv_shows.id = tv_genres.tv_show_id
 ORDER BY tv_shows.title, tv_genres.name;
