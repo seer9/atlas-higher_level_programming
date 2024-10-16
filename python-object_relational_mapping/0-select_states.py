dd15a3137c8ae25ed9f044d4787753c76c3a4b3d
clay <rpctropic13@gmail.com>
2024-10-16 01:49:08 +0000

    pycheck for line length

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -12,7 +12,8 @@ if __name__ == "__main__":
     password = sys.argv[2]
     database = sys.argv[3]
 
-    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
+    db = MySQLdb.connect(
+        host="localhost", port=3306, user=username, passwd=password, db=database)
     cursor = db.cursor()
 
     cursor.execute("SELECT id, name FROM states ORDER BY id ASC")
