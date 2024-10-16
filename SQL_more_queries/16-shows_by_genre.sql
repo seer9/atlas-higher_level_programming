d1796dd1238fa291e5c3039ebf4467e160fd9faf
clay <rpctropic13@gmail.com>
2024-10-16 01:25:36 +0000

    removed ascending order

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -2,4 +2,4 @@
 SELECT tv_shows.title, tv_genres.name
 FROM tv_shows
 LEFT JOIN tv_genres ON tv_shows.id = tv_genres.tv_show_id
-ORDER BY tv_shows.title ASC, tv_genres.name ASC;
+ORDER BY tv_shows.title, tv_genres.name;
