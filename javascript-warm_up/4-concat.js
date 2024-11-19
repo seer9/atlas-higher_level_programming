ac26803b2e4fdbc565ff9b689b2f088864cf7cd2
clay <rpctropic13@gmail.com>
2024-11-18 19:36:04 -0600

    using logic with each arguement

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
-// slice() to exclude the first two elements/arguements
+// exclude the first two arguments
 const args = process.argv.slice(2);
-// concatenate the first and second arguements properly
-const str = args[0].concat(' is ', args[1]);
+// concatenate the first and second arguments using the concat method
+const str = (args[0] || 'undefined').concat(' is ', args[1] || 'undefined');
 // print the string
 console.log(str);
