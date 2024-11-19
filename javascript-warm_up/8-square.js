0682afe342108fe16192f2fd2a17ee031c913e47
clay <rpctropic13@gmail.com>
2024-11-18 20:27:52 -0600

    repeat() makes duplicate rows within a loop

---
+++ javascript-warm_up/8-square.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+// get the first argument passed to the script, excluding the first two elements
+const arg = process.argv[2];
+// convert the argument to an integer
+const size = parseInt(arg);
+// check if the conversion was successful
+if (isNaN(size)) {
+  console.log('Missing size');
+} else {
+  // Loop to print the square
+  for (let i = 0; i < size; i++) {
+    console.log('X'.repeat(size));
+  }
+}
