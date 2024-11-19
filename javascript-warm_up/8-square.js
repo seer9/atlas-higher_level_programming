f084affa8fd6eece0566d881e034ae7203cb0f6a
clay <rpctropic13@gmail.com>
2024-11-18 20:28:51 -0600

    semistandard --fix

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -8,7 +8,7 @@ const size = parseInt(arg);
 if (isNaN(size)) {
   console.log('Missing size');
 } else {
-  // Loop to print the square
+  // loop to print the square
   for (let i = 0; i < size; i++) {
     console.log('X'.repeat(size));
   }
