81e96dfd6208797b286e83efcbf20da21efc6093
clay <rpctropic13@gmail.com>
2024-11-18 20:15:34 -0600

    semistandard check

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -3,10 +3,8 @@
 // get the first argument passed to the script,
 // excluding the first two elements
 const arg = process.argv[2];
-
 // convert the argument to an integer
 const num = parseInt(arg);
-
 // check if the conversion was successful and
 // print the appropriate message
 if (isNaN(num)) {
