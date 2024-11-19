05c507a516c96c7dc8f20d2aacf1283c9a8d69ce
clay <rpctropic13@gmail.com>
2024-11-18 20:09:42 -0600

    for loop through an array of text

---
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+// array of strings to print
+const text = [
+  'C is fun',
+  'Python is cool',
+  'JavaScript is amazing'
+];
+// loop through the array and print each message
+for (let i = 0; i < text.length; i++) {
+  console.log(text[i]);
+}
