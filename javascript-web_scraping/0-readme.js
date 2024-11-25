6c6edf28d12a7cc4e1d740034ab2371c51329a84
clay <rpctropic13@gmail.com>
2024-11-24 18:27:05 -0600

    temu cat function

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -4,8 +4,8 @@ const fs = require('fs');
 const filePath = process.argv[2];
 
 fs.readFile(filePath, 'utf8', (err, data) => {
-    if (err) {
-        return console.error(err);
-    }
-    console.log(data);
+  if (err) {
+    return console.error(err);
+  }
+  console.log(data);
 });
