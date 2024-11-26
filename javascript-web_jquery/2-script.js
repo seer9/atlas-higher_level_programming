fb99b34a6e2105590d210718c9be526365cb00ed
clay <rpctropic13@gmail.com>
2024-11-26 16:35:46 -0600

    add a listener function to jquery to change header color

--- javascript-web_jquery/2-script.js
+++ javascript-web_jquery/2-script.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 /* global $ */
 
-$(document).ready(function() {
-    $('#red_header').click(function() {
-        $('header').css('color', '#FF0000');
-    });
+$(document).ready(function () {
+  $('#red_header').click(function () {
+    $('header').css('color', '#FF0000');
+  });
 });
