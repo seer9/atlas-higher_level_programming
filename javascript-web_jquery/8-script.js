5e9cbcf144de77a7159452d441bf4d4467b7c57d
clay <rpctropic13@gmail.com>
2024-11-26 18:06:49 -0600

    getting the star wars series from the api to poulate the unlinked list

--- javascript-web_jquery/8-script.js
+++ javascript-web_jquery/8-script.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 /* global $ */
 
-$(document).ready(function() {
-    $.get('https://swapi-api.hbtn.io/api/films/?format=json', function(data) {
-        const movies = data.results;
-        movies.forEach(movie => {
-            $('#list_movies').append('<li>' + movie.title + '</li>');
-        });
+$(document).ready(function () {
+  $.get('https://swapi-api.hbtn.io/api/films/?format=json', function (data) {
+    const movies = data.results;
+    movies.forEach(movie => {
+      $('#list_movies').append('<li>' + movie.title + '</li>');
     });
+  });
 });
