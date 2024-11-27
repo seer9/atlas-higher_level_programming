6b31c9edc6622507a4e2875b9d477589884c0a84
clay <rpctropic13@gmail.com>
2024-11-26 18:27:44 -0600

    main

---
+++ javascript-web_jquery/9-script.js
@@ -0,0 +1,8 @@
+#!/usr/bin/js
+/* global $ */
+
+$(document).ready(function () {
+  $.get('https://hellosalut.stefanbohacek.dev/?lang=fr', function (data) {
+    $('#hello').text(data.hello);
+  });
+});
